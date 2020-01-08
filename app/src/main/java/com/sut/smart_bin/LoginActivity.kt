57f6a1aa486186f1638e71697c9767fc9d7f2136
thakorn09 <tanakon.rs@gmail.com>
2020-01-09 02:42:06 +0700

    Login Social in firebase

---
+++ app/src/main/java/com/sut/smart_bin/LoginActivity.kt
@@ -0,0 +1,76 @@
+package com.sut.smart_bin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.facebook.CallbackManager
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    var auth : FirebaseAuth? = null
+    var googleSignInClient : GoogleSignInClient? = null
+    var GOOGLE_LOGIN_CODE = 9100
+    var callbackManager : CallbackManager? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        google_sign_in_button.setOnClickListener {
+            //First step
+            googleLogin()
+        }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("311706871587-i8c8h8obvmr1pv14of860oc66vtp6jun.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+    }
+
+    fun googleLogin(){
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent,GOOGLE_LOGIN_CODE)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager?.onActivityResult(requestCode,resultCode,data)
+        if(requestCode == GOOGLE_LOGIN_CODE){
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result.isSuccess){
+                var account = result.signInAccount
+                //Second step
+                firebaseAuthWithGoogle(account)
+            }
+        }
+    }
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?){
+        var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                    task ->
+                if(task.isSuccessful){
+                    //Login
+                    moveMainPage(task.result?.user)
+                }else{
+                    //Show the error message
+                    Toast.makeText(this,task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    fun moveMainPage(user: FirebaseUser?){
+        if(user != null){
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/sut/smart_bin/MainActivity.kt
+++ app/src/main/java/com/sut/smart_bin/MainActivity.kt
@@ -15,60 +15,11 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var providers : List<AuthUI.IdpConfig>
-    val MY_REQUEST_CODE: Int = 7117
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //Tnit
-        providers = Arrays.asList<AuthUI.IdpConfig>(
-            AuthUI.IdpConfig.EmailBuilder().build()
-            //AuthUI.IdpConfig.FacebookBuilder().build(),
-           // AuthUI.IdpConfig.GoogleBuilder().build()
-
-        )
-
-        showSignInOption()
-
-        btn_sign_out.setOnClickListener {
-            AuthUI.getInstance().signOut(this@MainActivity)
-                .addOnCompleteListener {
-                    btn_sign_out.isEnabled=false
-                    showSignInOption()
-                }
-                .addOnFailureListener {
-                        e-> Toast.makeText(this@MainActivity,e.message,Toast.LENGTH_SHORT).show()
-                }
-        }
+        println("OK")
 
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == MY_REQUEST_CODE)
-        {
-            val response = IdpResponse.fromResultIntent(data)
-            if(resultCode == Activity.RESULT_OK){
-                val user = FirebaseAuth.getInstance().currentUser
-                Toast.makeText(this,""+user!!.email,Toast.LENGTH_SHORT).show()
-
-                btn_sign_out.isEnabled= true
-            }
-
-            else
-            {
-                Toast.makeText(this,""+response!!.error!!.message,Toast.LENGTH_SHORT).show()
-
-            }
-        }
-    }
-
-    private fun showSignInOption(){
-        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .setTheme(R.style.MyTheme).build(),MY_REQUEST_CODE)
-    }
-
 }
