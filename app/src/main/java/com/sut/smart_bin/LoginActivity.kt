e7f599e76fe526074d3fe417a1b278e69c9a0c3a
TungAo06 <chayanunzaza@gmail.com>
2020-01-09 05:02:39 +0700

    Login Google cannot facabook

--- app/src/main/java/com/sut/smart_bin/LoginActivity.kt
+++ app/src/main/java/com/sut/smart_bin/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.sut.smart_bin
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.widget.Toast
 import com.facebook.CallbackManager
 import com.google.android.gms.auth.api.Auth
@@ -10,15 +13,19 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class LoginActivity : AppCompatActivity() {
     var auth : FirebaseAuth? = null
     var googleSignInClient : GoogleSignInClient? = null
-    var GOOGLE_LOGIN_CODE = 9100
+    var GOOGLE_LOGIN_CODE = 9100;
     var callbackManager : CallbackManager? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,14 +40,22 @@ class LoginActivity : AppCompatActivity() {
             .requestIdToken("311706871587-i8c8h8obvmr1pv14of860oc66vtp6jun.apps.googleusercontent.com")
             .requestEmail()
             .build()
-        googleSignInClient = GoogleSignIn.getClient(this,gso)
 
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+        callbackManager = CallbackManager.Factory.create()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        moveMainPage(auth?.currentUser)
     }
 
     fun googleLogin(){
         var signInIntent = googleSignInClient?.signInIntent
         startActivityForResult(signInIntent,GOOGLE_LOGIN_CODE)
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         callbackManager?.onActivityResult(requestCode,resultCode,data)
@@ -53,6 +68,7 @@ class LoginActivity : AppCompatActivity() {
             }
         }
     }
+
     fun firebaseAuthWithGoogle(account : GoogleSignInAccount?){
         var credential = GoogleAuthProvider.getCredential(account?.idToken,null)
         auth?.signInWithCredential(credential)
@@ -63,11 +79,12 @@ class LoginActivity : AppCompatActivity() {
                     moveMainPage(task.result?.user)
                 }else{
                     //Show the error message
-                    Toast.makeText(this,task.exception?.message, Toast.LENGTH_LONG).show()
+                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
                 }
             }
     }
-    fun moveMainPage(user: FirebaseUser?){
+
+    fun moveMainPage(user:FirebaseUser?){
         if(user != null){
             startActivity(Intent(this,MainActivity::class.java))
             finish()
--- app/src/main/java/com/sut/smart_bin/MainActivity.kt
+++ app/src/main/java/com/sut/smart_bin/MainActivity.kt
@@ -4,12 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -17,9 +19,35 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        val user = FirebaseAuth.getInstance().currentUser
+            // Name, email address, and profile photo Url
+            val name = user?.displayName
+            val email = user?.email
+            val photoUrl = user?.photoUrl
+
+            // Check if user's email is verified
+            val emailVerified = user?.isEmailVerified
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            val uid = user?.uid
+
+
+        setContentView(R.layout.activity_main)
+        btn_sign_out.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            moveMainPage();
+        }
+        Log.d("TAG", "User profile updated." + name)
         println("OK")
+
+    }
+
+    fun moveMainPage(){
+            startActivity(Intent(this,LoginActivity::class.java))
+            finish()
 
     }
 }
